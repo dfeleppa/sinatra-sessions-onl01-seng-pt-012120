0b1e5b60ccb9ca26c020b73abee0932df7cfac0a
Daniel J Feleppa <dfeleppa@gmail.com>
2020-05-22 15:09:47 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -10,20 +10,23 @@ class App < Sinatra::Base
     content_type :txt
   end
   
-   get '/first_exercise' do
+  get '/' do
+    "Welcome to Sinatra Sessions! In this lab, we will be learning about the general principles behind a sessions cookie. In order to proceed, let's go to the '/first_exercise' path."
+  end
+
+  get '/first_exercise' do
     "Your first exercise will be to set your session key-value pair.\nIn the route: get '/set', write a line of code that sets the :foo key of the session hash equal to 'hello'.\nThen, navigate to the '/set' path."
   end
 
-
-  get '/set' do	  
-    session[:foo] = "hello"
-    if session[:foo] == 'hello'	
+  get '/set' do
+    # set the :foo key of the session hash equal to 'hello' here!
+    if session[:foo] == 'hello'
       redirect '/fetch'
     else
       "Session value has not been set!"
     end
   end
-  
+
   get '/fetch' do
     "You did it! session[:foo] value: #{session[:foo]}.\nMove on to Part II of this lab at '/second_exercise' "
   end
